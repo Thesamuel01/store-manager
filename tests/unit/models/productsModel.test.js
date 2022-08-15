309d15fb6a6acd8816efb7a2caf5d4287dc50b09
samuel santos <samaraujo28@gmail.com>
2022-08-15 17:07:24 -0300

    [test] implement products model mock on tests

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -1,36 +1,41 @@
 const { expect } = require('chai');
-
-const productsModel = {
-  getAll: () => {}
-};
+const { describe } = require('mocha');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const productsModel = require('../../../models/productsModel');
 
 describe('When search for all products on database', () => {
-  it('It should return an array', () => {
-    const result = productsModel.getAll();
+  before(() => {
+    const executeResult = [[{ id: 1, name: 'Martelo do Thor' }], []];
+
+    sinon.stub(connection, 'execute').resolves(executeResult);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+  it('It should return an array', async () => {
+    const result = await productsModel.getAll();
 
     expect(result).to.be.an('array');
   });
 
-  it('The array returned can not be empty', () => {
-    const result = productsModel.getAll();
+  it('The array returned can not be empty', async () => {
+    const result = await productsModel.getAll();
 
-    expect(result).to.be.empty();
+    expect(result).to.be.not.empty;
   });
 
-  it('It should return an array of objects', () => {
-    const result = productsModel.getAll();
+  it('It should return an array of objects', async () => {
+    const result = await productsModel.getAll();
+    const item = result[0];
 
-    expect(result).to.be.an('object');
+    expect(item).to.be.an('object');
   });
 
-  it('It should return an array of objects', () => {
-    const result = productsModel.getAll();
-
-    expect(result).to.be.an('object');
-  });
-
-  it('The objects from array must have "id" and "name" keys', () => {
-    const result = productsModel.getAll();
+  it('The objects from array must have "id" and "name" keys', async () => {
+    const result = await productsModel.getAll();
     const item = result[0];
 
     expect(item).to.include.all.keys('id', 'name');
